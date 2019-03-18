1947f90d073884c5a2022222d35dc0abaf2f5698
mnrotrmedic <33576579+mnrotrmedic@users.noreply.github.com>
2019-03-18 18:33:15 -0500

    broken

--- public/assets/script/script.js
+++ public/assets/script/script.js
@@ -1,30 +1,30 @@
-$("#submitButton").on("click", function (event) {
-    // Don't reload the page on click event
-    event.preventDefault();
+// $("#submitButton").on("click", function (event) {
+//     // Don't reload the page on click event
+//     event.preventDefault();
 
-    var newUser = {
-        name: $("#name").val().trim(),
-        photo: $("#photo").val().trim(),
-        scores: [
-            $("#sel1").val(),
-            $("#sel2").val(),
-            $("#sel3").val(),
-            $("#sel4").val(),
-            $("#sel5").val(),
-            $("#sel6").val(),
-            $("#sel7").val(),
-            $("#sel8").val(),
-            $("#sel9").val(),
-            $("#sel10").val()
-        ],
-    };
+//     var newUser = {
+//         name: $("#name").val().trim(),
+//         photo: $("#photo").val().trim(),
+//         scores: [
+//             $("#sel1").val(),
+//             $("#sel2").val(),
+//             $("#sel3").val(),
+//             $("#sel4").val(),
+//             $("#sel5").val(),
+//             $("#sel6").val(),
+//             $("#sel7").val(),
+//             $("#sel8").val(),
+//             $("#sel9").val(),
+//             $("#sel10").val()
+//         ],
+//     };
 
-    // $.post("app/data/friends"), newUser, function (data) {
-    //     if (data) { console.log("Winning") }
-    //     else { console.log("Not winning yet...") };
-    // };
+//     // $.post("app/data/friends"), newUser, function (data) {
+//     //     if (data) { console.log("Winning") }
+//     //     else { console.log("Not winning yet...") };
+//     // };
 
-    console.log(newUser);
-    $("form").trigger("reset");
+//     console.log(newUser);
+//     $("form").trigger("reset");
 
-});
+// });
