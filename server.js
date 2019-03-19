7b61caa1d50fcaecaf71ec7ff8f5e9e61a352deb
mnrotrmedic <33576579+mnrotrmedic@users.noreply.github.com>
2019-03-19 17:51:20 -0500

    small fix

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ var PORT = process.env.PORT || 3010;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
-
 //Allows use of static assets
 app.use(express.static('public'));
+
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
 //Lets-A-Go <--In Mario Voice
 app.listen(PORT, function () {
